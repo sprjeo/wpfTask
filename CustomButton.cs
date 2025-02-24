9e1a4dea552a1e3b65736c2aed4c05ebbbc039c1
Natalia <natashacherednikova@yandex.ru>
2025-02-24 18:22:40 +0300

    -

--- CustomButton.cs
+++ CustomButton.cs
@@ -4,42 +4,89 @@ using System.Windows.Media;
 
 namespace CustomControlsLibrary
 {
-    public class CustomButton : Button
+    public class CustomTextButton : Button
     {
-        // Зависимое свойство для фона кнопки
-        public static readonly DependencyProperty ButtonBackgroundProperty =
-            DependencyProperty.Register("ButtonBackground", typeof(Brush), typeof(CustomButton), new PropertyMetadata(Brushes.LightGray));
+        public static readonly DependencyProperty CustomBackgroundProperty =
+            DependencyProperty.Register("CustomBackground", typeof(Brush), typeof(CustomTextButton), new PropertyMetadata(Brushes.LightGray));
 
-        public Brush ButtonBackground
+        public Brush CustomBackground
         {
-            get { return (Brush)GetValue(ButtonBackgroundProperty); }
-            set { SetValue(ButtonBackgroundProperty, value); }
+            get { return (Brush)GetValue(CustomBackgroundProperty); }
+            set { SetValue(CustomBackgroundProperty, value); }
         }
 
-        // Зависимое свойство для цвета шрифта
-        public static readonly DependencyProperty FontColorProperty =
-            DependencyProperty.Register("FontColor", typeof(Brush), typeof(CustomButton), new PropertyMetadata(Brushes.Black));
-
-        public Brush FontColor
+        public static readonly DependencyProperty CustomForegroundProperty =
+            DependencyProperty.Register("CustomForeground", typeof(Brush), typeof(CustomTextButton), new PropertyMetadata(Brushes.Black));
+        public Brush CustomForeground
         {
-            get { return (Brush)GetValue(FontColorProperty); }
-            set { SetValue(FontColorProperty, value); }
+            get { return (Brush)GetValue(CustomForegroundProperty); }
+            set { SetValue(CustomForegroundProperty, value); }
         }
 
-        // Зависимое свойство для размера шрифта
-        public static readonly DependencyProperty FontSizeProperty =
-            DependencyProperty.Register("FontSize", typeof(double), typeof(CustomButton), new PropertyMetadata(12.0));
+        public static readonly DependencyProperty CustomFontSizeProperty =
+            DependencyProperty.Register("CustomFontSize", typeof(double), typeof(CustomTextButton), new PropertyMetadata(14.0));
 
-        public double FontSize
+        public double CustomFontSize
         {
-            get { return (double)GetValue(FontSizeProperty); }
-            set { SetValue(FontSizeProperty, value); }
+            get { return (double)GetValue(CustomFontSizeProperty); }
+            set { SetValue(CustomFontSizeProperty, value); }
         }
 
-        // Конструктор
-        static CustomButton()
+        static CustomTextButton()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomButton), new FrameworkPropertyMetadata(typeof(CustomButton)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomTextButton), new FrameworkPropertyMetadata(typeof(CustomTextButton)));
         }
     }
+    public class CustomImageButton : Button
+    {
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(CustomImageButton), new PropertyMetadata(null));
+
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty XScaleProperty =
+            DependencyProperty.Register("XScale", typeof(double), typeof(CustomImageButton), new PropertyMetadata(1.0));
+
+        public double XScale
+        {
+            get { return (double)GetValue(XScaleProperty); }
+            set { SetValue(XScaleProperty, value); }
+        }
+
+        public static readonly DependencyProperty YScaleProperty =
+            DependencyProperty.Register("YScale", typeof(double), typeof(CustomImageButton), new PropertyMetadata(1.0));
+
+        public double YScale
+        {
+            get { return (double)GetValue(YScaleProperty); }
+            set { SetValue(YScaleProperty, value); }
+        }
+
+        static CustomImageButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomImageButton), new FrameworkPropertyMetadata(typeof(CustomImageButton)));
+        }
+    }
+    public class CustomScrollViewer : ScrollViewer
+    {
+        public static readonly DependencyProperty ScrollBarColorProperty =
+            DependencyProperty.Register("ScrollBarColor", typeof(Brush), typeof(CustomScrollViewer), new PropertyMetadata(Brushes.Gray));
+
+        public Brush ScrollBarColor
+        {
+            get { return (Brush)GetValue(ScrollBarColorProperty); }
+            set { SetValue(ScrollBarColorProperty, value); }
+        }
+
+        static CustomScrollViewer()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomScrollViewer), new FrameworkPropertyMetadata(typeof(CustomScrollViewer)));
+        }
+    }
+
+
 }
